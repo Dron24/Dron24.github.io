ea8e25ba5365f6b714974f08c85e393ac3c9c98a
Dron24 <stepanoff888@yandex.ru>
2024-01-09 10:38:42 +0500

    1

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const app = express();
 const mysql = require('mysql2');
 
 // Подключение к базе данных
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'nice.carlee.ru', // Адрес сервера базы данных
   user: 'dron24_24', // Имя пользователя базы данных
   password: '11111', // Пароль пользователя базы данных
-  database: 'dron24_24' // Имя базы данных
+  database: 'dron24_24', // Имя базы данных
+  connectionLimit: 10 // Максимальное количество соединений в пуле
 });
 
 // Проверка подключения к базе данных
-connection.connect((err) => {
-    if (err) {
-      console.error('Ошибка подключения к базе данных: ' + err.stack);
-      return;
-    }
-    console.log('Подключение к базе данных успешно установлено');
-  });
-  
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Ошибка подключения к базе данных: ' + err.stack);
+    return;
+  }
+  console.log('Подключение к базе данных успешно установлено');
+
   // Обработка запросов к базе данных
   app.get('/data', (req, res) => {
     connection.query('SELECT * FROM table_name', (error, results) => {
@@ -30,3 +30,12 @@ connection.connect((err) => {
       res.json(results);
     });
   });
+
+  // Освобождение соединения после использования
+  connection.release();
+});
+
+// Запуск сервера
+app.listen(3000, () => {
+  console.log('Сервер запущен на порту 3000');
+});
